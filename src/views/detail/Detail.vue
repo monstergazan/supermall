dbbf8a8e61df95c751a0b8aee395409d1d78ed42
“root” <873676545@qq.com>
2021-05-10 23:28:54 +0800

    184

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -3,6 +3,7 @@
     <detail-navbar/>
     <detail-swiper :top-images="topImages"/>
     <detail-base-info :goods="goods"/>
+    <DetailShopInfo :shop="shop"></DetailShopInfo>
   </div>
 </template>
 
@@ -10,26 +11,24 @@
   import DetailNavbar from "./childComps/DetailNavbar";
   import DetailSwiper from "./childComps/DetailSwiper";
   import DetailBaseInfo from "./childComps/DetailBaseInfo";
-  import {getDetail, Goods} from 'network/detail';
+  import DetailShopInfo from "./childComps/DetailShopInfo";
+  import {getDetail, Goods,Shop} from 'network/detail';
 
 
   export default {
     components: {
       DetailNavbar,
       DetailSwiper,
-      DetailBaseInfo
+      DetailBaseInfo,
+      DetailShopInfo
     },
     name: "Detail",
     data(){
       return {
         iid: null,
         topImages: [],
-        goods: {
-          type: Object,
-          default(){
-            return {}
-          }
-        }
+        goods: {},
+        shop: {}
       }
     },
     created() {
@@ -37,11 +36,15 @@
       this.iid = this.$route.params.iid
       //2.根据iid请求详细数据
       getDetail(this.iid).then(res=>{
-        console.log(res);
+        // console.log(res);
+        // 封装到data
         const data = res.result
+
         this.topImages = data.itemInfo.topImages
         this.goods = new Goods(data.itemInfo,data.columns,data.shopInfo.services)
-        console.log(this.goods);
+        // console.log(this.goods);
+        //3.创建店铺信息的对象
+        this.shop = new Shop(data.shopInfo)
       })
     }
   }
